3cea00057094e01b700862b96e5e49bc8cf0be63
Donny Techiera <DonnyTech20@gmail.com>
2019-09-10 14:26:27 -0400

    added time & date to ui

---
+++ date.js
@@ -0,0 +1,15 @@
+let today = new Date();
+let hourNow = today.getHours();
+let greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good Evening & Welcome!';
+} else if (hourNow > 12) {
+greeting = "Good Afternoon & Welcome!";
+} else if (hourNow > 0) {
+    greeting = 'Good Morning & Welcome!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.getElementById('first').innerHTML = today;
